4c27e9f51471bf7b510520905cb1807e77953e11
Marco Pauws <mpauws@gmail.com>
2020-09-16 00:23:03 +0200

    CSS, reordering and adding content

--- client/app.js
+++ client/app.js
@@ -1,31 +1,31 @@
 // https://www.youtube.com/watch?v=J3KGuDRr0tE
 
 const API_KEY = "4d772a2f024b736731088cbd1691d445";
-const charactersURL = `https://gateway.marvel.com:443/v1/public/events/29/characters?limit=60&apikey=${API_KEY}`;
+const charactersInfinityURL = `https://gateway.marvel.com:443/v1/public/events/29/characters?limit=60&apikey=${API_KEY}`;
 
 // IMAGE VARIANTS: https://developer.marvel.com/documentation/images
 const imgRatioPortrait = "portrait_incredible"; // 216 x 324px
 const imgRatioSquare = "standard_fantastic"; // 250 x 250px
 const imgRatioLandscape = "landscape_amazing"; // 250 x 156px
 
-const audioElement = new Audio("sounds/intro.mp3");
-/* audioElement.play(); */
-
 const snapElement = document.getElementById("snap-button");
+const characterContainer = document.querySelector(".character-container");
 
-function getCharacterData() {
-   // Local Storage (55 min)
+// const audioElement = new Audio("sounds/intro.mp3");
+// audioElement.play();
+
+const getCharacterData = () => {
    if (localStorage.getCharacterData) {
       return Promise.resolve(JSON.parse(localStorage.characterData));
    }
 
-   return fetch(charactersURL)
+   return fetch(charactersInfinityURL)
       .then((response) => response.json())
       .then((data) => {
-         localStorage.charactersURL = JSON.stringify(data);
+         localStorage.charactersInfinityURL = JSON.stringify(data);
          return data;
       });
-}
+};
 
 // To hide groups & Thanos (Avengers, Fantastic Four, X-Men)
 const hiddenCharacters = {
@@ -35,27 +35,33 @@ const hiddenCharacters = {
    1009299: true,
 };
 
-function addCharactersToPage(characterData) {
-   // console.log(characterData.data.results);
+const addCharactersToPage = (characterData) => {
+   console.log(characterData);
+   // Header
+   const seriesNameElement = document.querySelector(".series-header");
+   seriesNameElement.textContent = "Infinity Gauntlet";
 
    characterData.data.results.forEach((character) => {
       const characterImage = `${character.thumbnail.path}/${imgRatioSquare}.${character.thumbnail.extension}`;
-      const characterContainer = document.querySelector(".character");
-      const newDiv = document.createElement("div");
-      const newImg = document.createElement("img");
-      const characterNameElement = document.createElement("h4");
       const characterName = character.name.replace(/\(.*\)/, "");
+      const newDiv = document.createElement("div");
+      const characterNameElement = document.createElement("h4");
+      const newImg = document.createElement("img");
+      const newParagraph = document.createElement("p");
 
       if (!hiddenCharacters[character.id]) {
-         newImg.src = characterImage;
          characterContainer.appendChild(newDiv);
          newDiv.appendChild(newImg);
+         newDiv.classList.add("character-item");
+         newImg.src = characterImage;
          characterNameElement.textContent = characterName;
          newDiv.appendChild(characterNameElement);
+         newParagraph.textContent = "Appeared in x comics";
+         newDiv.appendChild(newParagraph);
       }
    });
-}
+};
 
 getCharacterData().then(addCharactersToPage);
 
-snapElement.addEventListener("click", () => {});
+// snapElement.addEventListener("click", () => {});
