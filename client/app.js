a7e758e1fb67fdbbce7920bc5e46899559a53069
Marco Pauws <mpauws@gmail.com>
2020-09-15 21:34:14 +0200

    extra rendering

--- client/app.js
+++ client/app.js
@@ -8,9 +8,13 @@ const imgRatioPortrait = "portrait_incredible"; // 216 x 324px
 const imgRatioSquare = "standard_fantastic"; // 250 x 250px
 const imgRatioLandscape = "landscape_amazing"; // 250 x 156px
 
-// Local Storage (55 min)
+const audioElement = new Audio("sounds/intro.mp3");
+/* audioElement.play(); */
+
+const snapElement = document.getElementById("snap-button");
 
 function getCharacterData() {
+   // Local Storage (55 min)
    if (localStorage.getCharacterData) {
       return Promise.resolve(JSON.parse(localStorage.characterData));
    }
@@ -23,8 +27,35 @@ function getCharacterData() {
       });
 }
 
+// To hide groups & Thanos (Avengers, Fantastic Four, X-Men)
+const hiddenCharacters = {
+   1009652: true,
+   1009165: true,
+   1009726: true,
+   1009299: true,
+};
+
 function addCharactersToPage(characterData) {
-   console.log(characterData.data.results);
+   // console.log(characterData.data.results);
+
+   characterData.data.results.forEach((character) => {
+      const characterImage = `${character.thumbnail.path}/${imgRatioSquare}.${character.thumbnail.extension}`;
+      const characterContainer = document.querySelector(".character");
+      const newDiv = document.createElement("div");
+      const newImg = document.createElement("img");
+      const characterNameElement = document.createElement("h4");
+      const characterName = character.name.replace(/\(.*\)/, "");
+
+      if (!hiddenCharacters[character.id]) {
+         newImg.src = characterImage;
+         characterContainer.appendChild(newDiv);
+         newDiv.appendChild(newImg);
+         characterNameElement.textContent = characterName;
+         newDiv.appendChild(characterNameElement);
+      }
+   });
 }
 
 getCharacterData().then(addCharactersToPage);
+
+snapElement.addEventListener("click", () => {});
