4897873bcca30597ba3593c055bf6565aafea65a
Marco Pauws <mcpauws@Marcos-MBP-3.fritz.box>
2020-09-21 19:18:26 +0200

    changing featured character feature

--- client/app.js
+++ client/app.js
@@ -9,6 +9,84 @@ let randomOffset = Math.floor(Math.random() * 1488);
 // console.log(randomOffset);
 const featuredChar = `${BASE_URL}443/v1/public/characters?limit=5&offset=${randomOffset}${API_KEY}`;
 
+const favoriteCharacterIDs = [
+  1009146,
+  1009165,
+  1017109,
+  1009220,
+  1009220,
+  1010676,
+  1009262,
+  1009297,
+  1017110,
+  1009299,
+  1011299,
+  1009619,
+  1017108,
+  1009351,
+  1017098,
+  1017107,
+  1016839,
+  1009368,
+  1017104,
+  1009538,
+  1017577,
+  1017099,
+  1010744,
+  1009571,
+  1009577,
+  1017111,
+  1009583,
+  1009588,
+  1009592,
+  1011223,
+  1009609,
+  1011347,
+  1009610,
+  1012200,
+  1011114,
+  1016181,
+  1011010,
+  1009608,
+  1010794,
+  1010733,
+  1009629,
+  1010979,
+  1009652,
+  1017103,
+  1009662,
+  1010983,
+  1009664,
+  1017576,
+  1017106,
+  1011025,
+  1010820,
+  1010369,
+  1016825,
+  1009685,
+  1010350,
+  1009663,
+  1010788,
+  1011128,
+  1009697,
+  1009699,
+  1010990,
+  1010991,
+  1009707,
+  1010992,
+  1009718,
+  1011006,
+  1009719,
+  1009720,
+  1009725,
+  1009726,
+];
+const randomArrayIndex = Math.floor(
+  Math.random() * favoriteCharacterIDs.length - 1
+);
+
+console.log(favoriteCharacterIDs[randomArrayIndex]);
+
 // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 // SERIES CHARACTER URLs
 const charactersInfinity = `${BASE_URL}443/v1/public/events/29/characters?orderBy=name&limit=60${API_KEY}`;
@@ -46,114 +124,106 @@ const characterContainer = document.querySelector(".character-container");
 const spinner = document.getElementById("spinner");
 const featuredCharImg = document.querySelector(".featured-char-image");
 const featuredCharNameDiv = document.querySelector(".featured-char-name");
-const featuredCharDescrDiv = document.querySelector(".featured-char-description");
+const featuredCharDescrDiv = document.querySelector(
+  ".featured-char-description"
+);
 
 // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 // SPINNER FUNCTIONALITY
 const spinnerVisible = () => {
-   spinner.style.display = "block";
-   setTimeout(() => {
-      spinner.style.display = "none";
-   }, 5000);
+  spinner.style.display = "block";
+  setTimeout(() => {
+    spinner.style.display = "none";
+  }, 5000);
 };
 
 const spinnerHidden = () => {
-   spinner.style.display = "none";
+  spinner.style.display = "none";
 };
 
 // TO DO: localStorage!!!
 
 const handleClick = (series) => {
-   seriesNameElement.textContent = `Characters of ${series}`;
-   if (series === "Infinity Gauntlet") {
-      getCharacters(charactersInfinity).then(addCharactersToPage);
-   } else if (series === "Age of Ultron") {
-      getCharacters(charactersAgeUltron).then(addCharactersToPage);
-   } else if (series === "Civil War") {
-      getCharacters(charactersCivilWar).then(addCharactersToPage);
-   }
+  seriesNameElement.textContent = `Characters of ${series}`;
+  if (series === "Infinity Gauntlet") {
+    getCharacters(charactersInfinity).then(addCharactersToPage);
+  } else if (series === "Age of Ultron") {
+    getCharacters(charactersAgeUltron).then(addCharactersToPage);
+  } else if (series === "Civil War") {
+    getCharacters(charactersCivilWar).then(addCharactersToPage);
+  }
 };
 
 // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 // FETCH CHARACTERS
 const getCharacters = (seriesURL) => {
-   characterContainer.textContent = "";
-   spinnerVisible();
-   return fetch(seriesURL)
-      .then((response) => response.json())
-      .then((data) => {
-         spinnerHidden();
-         return data;
-      });
+  characterContainer.textContent = "";
+  spinnerVisible();
+  return fetch(seriesURL)
+    .then((response) => response.json())
+    .then((data) => {
+      spinnerHidden();
+      return data;
+    });
 };
 
 // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 // ADD CHARACTERS TO DOM
 const addCharactersToPage = (characterData) => {
-   // console.log(characterData);
-   console.log(characterData.data.results);
+  // console.log(characterData);
+  console.log(characterData.data.results);
 
-   characterData.data.results.sort(function (a, b) {
-      return b.comics.available - a.comics.available;
-   });
+  characterData.data.results.sort(function (a, b) {
+    return b.comics.available - a.comics.available;
+  });
 
-   characterData.data.results.forEach((character) => {
-      const characterImage = `${character.thumbnail.path}/${imgRatioSquare}.${character.thumbnail.extension}`;
-      const comicBookFrequency = `${character.comics.available}`;
-      const characterName = character.name.replace(/\(.*\)/, "");
-      const newDiv = document.createElement("div");
-      const characterNameElement = document.createElement("h4");
-      const imgContainer = document.createElement("div");
-      const newImg = document.createElement("img");
-      const newParagraph = document.createElement("p");
+  characterData.data.results.forEach((character) => {
+    const characterImage = `${character.thumbnail.path}/${imgRatioSquare}.${character.thumbnail.extension}`;
+    const comicBookFrequency = `${character.comics.available}`;
+    const characterName = character.name.replace(/\(.*\)/, "");
+    const newDiv = document.createElement("div");
+    const characterNameElement = document.createElement("h4");
+    const imgContainer = document.createElement("div");
+    const newImg = document.createElement("img");
+    const newParagraph = document.createElement("p");
 
-      characterContainer.appendChild(newDiv);
-      newDiv.appendChild(imgContainer);
-      imgContainer.appendChild(newImg);
-      imgContainer.classList.add("image-container");
-      newDiv.classList.add("character-item");
-      newImg.src = characterImage;
-      characterNameElement.textContent = characterName;
-      newDiv.appendChild(characterNameElement);
-      newParagraph.textContent = `Appeared in ${comicBookFrequency} comics`;
-      newDiv.appendChild(newParagraph);
+    characterContainer.appendChild(newDiv);
+    newDiv.appendChild(imgContainer);
+    imgContainer.appendChild(newImg);
+    imgContainer.classList.add("image-container");
+    newDiv.classList.add("character-item");
+    newImg.src = characterImage;
+    characterNameElement.textContent = characterName;
+    newDiv.appendChild(characterNameElement);
+    newParagraph.textContent = `Appeared in ${comicBookFrequency} comics`;
+    newDiv.appendChild(newParagraph);
 
-      // TO DO: display description of character
-      newDiv.addEventListener("click", () => {
-         console.log(character.description);
-      });
-   });
+    // TO DO: display description of character
+    newDiv.addEventListener("click", () => {
+      console.log(character.description);
+    });
+  });
 };
 
 const addFeaturedCharToPage = (characterData) => {
-   const characterDataVar = characterData.data.results;
-   let indexNumber = 0;
+  const characterDataVar = characterData.data.results;
 
-   characterDataVar[indexNumber] ? (indexNumber = 0) : indexNumber++;
+  characterDataVar.sort(function (a, b) {
+    return b.comics.available - a.comics.available;
+  });
 
-   characterDataVar.sort(function (a, b) {
-      return b.comics.available - a.comics.available;
-   });
+  console.log(characterDataVar[0]);
 
-   console.log(characterDataVar[0]);
-   // const characterImage = `${character.thumbnail.path}/${imgRatioSquare}.${character.thumbnail.extension}`;
-   // console.log("Featured Character Name: ", characterData.data.results[0].name);
-   /* console.log(
-      "Featured Character Img URL: ",
-      `${characterData.data.results[0].thumbnail.path}/${imgRatioSquare}.${characterData.data.results[0].thumbnail.extension}`
-   ); */
-   // console.log("Featured Character Description: ", characterData.data.results[0].description);
+  const newImg = document.createElement("img");
+  const featuredCharName = document.createElement("h2");
+  const newParagraph = document.createElement("p");
 
-   const newImg = document.createElement("img");
-   const featuredCharName = document.createElement("h2");
-   const newParagraph = document.createElement("p");
-
-   newImg.src = `${characterDataVar[indexNumber].thumbnail.path}/${imgRatioSquare}.${characterData.data.results[indexNumber].thumbnail.extension}`;
-   featuredCharImg.appendChild(newImg);
-   featuredCharName.textContent = characterDataVar[indexNumber].name;
-   featuredCharNameDiv.appendChild(featuredCharName);
-   newParagraph.textContent = characterDataVar[indexNumber].description;
-   featuredCharDescrDiv.appendChild(newParagraph);
+  newImg.src = `${characterDataVar[0].thumbnail.path}/${imgRatioSquare}.${characterDataVar[0].thumbnail.extension}`;
+  featuredCharImg.appendChild(newImg);
+  featuredCharName.textContent = characterDataVar[0].name;
+  featuredCharNameDiv.appendChild(featuredCharName);
+  newParagraph.textContent = characterDataVar[0].description;
+  featuredCharDescrDiv.appendChild(newParagraph);
 };
 
 getCharacters(featuredChar).then(addFeaturedCharToPage);
